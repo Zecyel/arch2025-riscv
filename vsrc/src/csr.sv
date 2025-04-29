d47612221f3b05701680121a5949e99def18e58e
Zecyel <i@zecyel.xyz>
2025-04-29 13:59:52 +0800

    feat: impl auto-increment of mcycle register

--- vsrc/src/csr.sv
+++ vsrc/src/csr.sv
@@ -37,21 +37,30 @@ module csr
             csr_reg.mcycle <= 0;
             csr_reg.mhartid <= 0;
             csr_reg.satp <= 0;
-        end else if (csr_write_enable) begin
-            unique case (csr_dest_addr)
-                CSR_MSTATUS: csr_reg.mstatus <= csr_write_data & MSTATUS_MASK;
-                CSR_MTVEC: csr_reg.mtvec <= csr_write_data & MTVEC_MASK;
-                CSR_MIP: csr_reg.mip <= csr_write_data & MIP_MASK;
-                CSR_MIE: csr_reg.mie <= csr_write_data;
-                CSR_MSCRATCH: csr_reg.mscratch <= csr_write_data;
-                CSR_MCAUSE: csr_reg.mcause <= csr_write_data;
-                CSR_MTVAL: csr_reg.mtval <= csr_write_data;
-                CSR_MEPC: csr_reg.mepc <= csr_write_data;
-                CSR_MCYCLE: csr_reg.mcycle <= csr_write_data;
-                CSR_MHARTID: csr_reg.mhartid <= csr_write_data;
-                CSR_SATP: csr_reg.satp <= csr_write_data;
-                default: begin end
-            endcase
+        end else begin
+            if (! csr_write_enable) begin
+                csr_reg.mcycle <= csr_reg.mcycle + 1;
+            end else if (csr_write_enable) begin
+                
+                if (csr_dest_addr != CSR_MCYCLE) begin
+                    csr_reg.mcycle <= csr_reg.mcycle + 1;
+                end
+
+                unique case (csr_dest_addr)
+                    CSR_MSTATUS: csr_reg.mstatus <= csr_write_data & MSTATUS_MASK;
+                    CSR_MTVEC: csr_reg.mtvec <= csr_write_data & MTVEC_MASK;
+                    CSR_MIP: csr_reg.mip <= csr_write_data & MIP_MASK;
+                    CSR_MIE: csr_reg.mie <= csr_write_data;
+                    CSR_MSCRATCH: csr_reg.mscratch <= csr_write_data;
+                    CSR_MCAUSE: csr_reg.mcause <= csr_write_data;
+                    CSR_MTVAL: csr_reg.mtval <= csr_write_data;
+                    CSR_MEPC: csr_reg.mepc <= csr_write_data;
+                    CSR_MCYCLE: csr_reg.mcycle <= csr_write_data;
+                    CSR_MHARTID: csr_reg.mhartid <= csr_write_data;
+                    CSR_SATP: csr_reg.satp <= csr_write_data;
+                    default: begin end
+                endcase
+            end
         end
     end
 
